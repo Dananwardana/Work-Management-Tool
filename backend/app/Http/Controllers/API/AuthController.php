962b3769cbd8b1a8a795bc4cddf18eb40a6bd168
Alya Nabila <alyanabilaaz@student.uns.ac.id>
2026-02-01 00:00:07 +0700

    auth done

--- backend/app/Http/Controllers/API/AuthController.php
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -26,6 +26,6 @@ class AuthController extends Controller
         request()->session()->invalidate();
         request()->session()->regenerateToken();
 
-        return response()->json(['message'=>'Logged out']);
+        return response()->json(['message'=>'Logged out!']);
     }
 }
